36cf0995fd98418d4ce3f7255eb2f076ac2aca49
dev-ths <alphavo.ths@gmail.com>
2024-02-01 15:00:38 -0800

    update

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -9,5 +9,35 @@
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
 
 const courseName = "typescript";
+const courseName2 = 'typescript';
+const courseName3 = `typescript`
 
-const stuff = ''
+const amount = 10
+const fraction  = 10.5
+const oneThousand = 1e3
+
+const allPermissionsLinux = 0o777
+const hexByte = 0xff
+
+const binary = 0b0100001
+const bigInt = 9000n
+
+const boolTrue = true
+const boolFalse = false
+
+// completely missing, or could not be found
+const missing = undefined
+
+// we know it's missing, value intentionally empty, aka null
+const empty = null
+
+let someNum = 0
+someNum = 1
+someNum = 2
+
+let hello:string;
+
+//hello = 1
+hello = 'hi'
+
+let what
