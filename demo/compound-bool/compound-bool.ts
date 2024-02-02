f36445690489d6fafd29709555e8b99ef53171f4
dev-ths <alphavo.ths@gmail.com>
2024-02-01 16:28:31 -0800

    update

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -5,4 +5,14 @@ let writing = true
 let reading = !writing
 
 const rating = 9
-const favouriteMovie =
+const favouriteMovie = false
+
+const suggestMovie:boolean = rating > 8 || favouriteMovie
+assert.equal(suggestMovie,true)
+
+const packageWeight = 30
+const packageLength = 20
+
+const feeExemption = false
+
+const payExtraFee = !feeExemption && (packageWeight > 25)
